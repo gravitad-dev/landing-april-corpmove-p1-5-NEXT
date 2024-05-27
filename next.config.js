fb3fddc19533b765b2997a3e6ebc16b393664d44
Wilder Ap <wilderasprilla321@gmail.com>
2024-05-27 12:02:41 -0500

    iFrame config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Esto aplica los encabezados a todas las rutas
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://jim-henry-landing.vercel.app/", // Reemplaza con el dominio que va a contener el iframe
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = {
   nextConfig,
